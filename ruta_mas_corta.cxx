7a7d4c652d92d8874dfb4d1951b74d3e8f38b01b
Juan Mendoza <mendozajuanjose348@gmail.com>
2025-08-24 09:22:35 -0500

    Delete ruta_mas_corta.cxx

--- ruta_mas_corta.cxx
+++
@@ -1,8 +0,0 @@
-#include "COMMAND_FUNCTIONS.h"
-
-void ruta_mas_corta(std::string descripcion_secuencia, int i, int j, int x, int y){
-
-    std::cout<<"Exito2 "<<descripcion_secuencia<<" i= "<<i<<" j= "<<j<<" x= "<<x<<" y= "<<y<<"\n";
-
-}
- 
