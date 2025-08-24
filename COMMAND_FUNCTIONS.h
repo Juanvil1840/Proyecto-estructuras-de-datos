777dd629fccf83c1c017269b81644f2f7dad6b13
Juan Mendoza <mendozajuanjose348@gmail.com>
2025-08-24 11:42:59 -0500

    Update COMMAND_FUNCTIONS.h

--- COMMAND_FUNCTIONS.h
+++ COMMAND_FUNCTIONS.h
@@ -1,26 +1,33 @@
-#ifndef __COMMAND_FUNCTIONS__H__
-#define __COMMAND_FUNCTIONS__H__
+#ifndef __SECUENCIAS__H__
+#define __SECUENCIAS__H__
 #include<iostream>
+#include<vector>
+#include<list>
 
 //para primera entrega todas las funciones no cuentan con retorno
-void cargar(std::string nombre_archivo);
 
-void listar_secuencias();
+class Secuencias{
 
-void histograma(std::string descripcion_secuencia);
+    
+protected:
+    std::string descripcion;
+    std::vector< std::string >linea_secuencia;
+public:
+    //constructor
+    Secuencias();
+    // Destructor
+    virtual ~Secuencias();
+    std::list< Secuencias > cargar(std::string nombre_archivo);
+    void listar_secuencias();
+    void histograma(std::string descripcion_secuencia);
+    void es_subsecuencia(std::string subsecuencia);
+    void enmascarar(std::string subsecuencia);
+    void guardar(std:: string nombre_archivo);
+    void codificar(std:: string nombre_archivo);
+    void decodificar(std::string nombre_archivo);
+    void ruta_mas_corta(std::string descripcion_secuencia, int i, int j, int x, int y);
+    void base_remota(std::string base_remota, int i, int j);
 
-void es_subsecuencia(std::string subsecuencia);
+};
 
-void enmascarar(std::string subsecuencia);
-
-void guardar(std:: string nombre_archivo);
-
-void codificar(std:: string nombre_archivo);
-
-void decodificar(std::string nombre_archivo);
-
-void ruta_mas_corta(std::string descripcion_secuencia, int i, int j, int x, int y);
-
-void base_remota(std::string base_remota, int i, int j);
-
-#endif // __COMMAND_FUNCTIONS__H__
+#endif // __SECUENCIAS__H__
