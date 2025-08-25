b95c76d2ec96006f47877e64947f82d4bdce867f
linafperez <lifepega2013@gmail.com>
2025-08-25 13:34:14 -0500

    Update Secuencia.h

--- Secuencia.h
+++ Secuencia.h
@@ -7,21 +7,26 @@ class Secuencia {
   public:
     Secuencia();
     virtual ~Secuencia();
-    std::string ObtenerDescripcion();
+    std::string& ObtenerDescripcion();
     std::vector< std::string >& ObtenerLineasSecuencia();
     int ObtenerNumbases();
     int ObtenerNumcodigos();
+    std::vector< char >& ObtenerCodigos();
     void FijarDescripcion(std::string ndescripcion);
     void FijarLineasSecuencia(std::vector< std::string > n_lineas_secuencia );
     void FijarNumbases(int n_numbases);
     void FijarNumcodigos(int n_numcodigos);
+    void FijarCodigos(std::vector< char > nCodigos);
     void AgregarLineaSecuencia(std::string linea);
+    void EstablecerCodigosYBases();
 
   protected:
     std::string descripcion;
     std::vector< std::string > lineas_secuencia;
     int numbases;
     int numcodigos;
+    std::vector< char > codigos;
+
 };
 
 #endif // __SISTEMA__H__
